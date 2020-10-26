ec78591b6cad82355154b524cf20b3f19616f2c8
zqc970816 <1245910840@qq.com>
2020-10-26 13:53:43 +0800

    updated

--- Training/evaluation/__init__.py
+++ Training/evaluation/__init__.py
@@ -3,7 +3,9 @@
 Author: TJUZQC
 Date: 2020-10-26 13:36:26
 LastEditors: TJUZQC
-LastEditTime: 2020-10-26 13:37:11
+LastEditTime: 2020-10-26 13:53:30
 Description: None
 '''
 from .eval import *
+from .pixel_accuracy import *
+from .dice_loss import *
