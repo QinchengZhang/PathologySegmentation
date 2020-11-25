181e091d4dfd8dcb8315b157a65c6fe2d8c27297
zqc970816 <1245910840@qq.com>
2020-11-25 14:24:37 +0800

    updated

--- Training/paddlepaddle/paddleseg/cvlibs/config.py
+++ Training/paddlepaddle/paddleseg/cvlibs/config.py
@@ -208,7 +208,8 @@ class Config(object):
                 if key == 'types':
                     self._losses['types'] = []
                     for item in args['types']:
-                        item['ignore_index'] = self.train_dataset.ignore_index
+                        if not (isinstance(item['ignore_index'], str) and item['ignore_index'].lower() == 'false'):
+                            item['ignore_index'] = self.train_dataset.ignore_index
                         self._losses['types'].append(self._load_object(item))
                 else:
                     self._losses[key] = val
