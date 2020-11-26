4eb8961bbf0132a8fff0189aa159f4fec5fe2572
zqc970816 <1245910840@qq.com>
2020-11-26 14:41:24 +0800

    updated

--- Training/paddlepaddle/paddleseg/core/train.py
+++ Training/paddlepaddle/paddleseg/core/train.py
@@ -120,6 +120,7 @@ def train(model,
                 # ddp_model.apply_collective_grads()
             else:
                 logits = model(images)
+                print(logits.shape)
                 loss = loss_computation(logits, labels, losses)
                 loss.backward()
             optimizer.step()
